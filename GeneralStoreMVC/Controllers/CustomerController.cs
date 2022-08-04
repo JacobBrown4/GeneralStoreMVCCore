059da194a288a4e17dc6607b5212a5e5a313fd13
Jacob <Jacobbrown4@gmail.com>
2022-08-04 12:13:27 -0400

    Initial

---
+++ GeneralStoreMVC/Controllers/CustomerController.cs
@@ -0,0 +1,97 @@
+﻿using GeneralStoreMVC.Models.Customer;
+using GeneralStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeneralStoreMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _service;
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _service.ListCustomers();
+            return View(customers);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var customer = await _service.GetCustomerById(id);
+            if (customer == null)
+                return NotFound();
+
+            return View(customer);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CustomerCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMsg"] = "Model State is Invalid";
+            }
+
+            bool wasCreated = await _service.CreateCustomer(model);
+
+            if (!wasCreated)
+                return RedirectToAction(nameof(Index));
+
+            TempData["ErrorMsg"] = "Unable to save to database. Please try again later.";
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            CustomerDetail customer = await _service.GetCustomerById(id);
+
+            if (customer == null) return NotFound();
+
+            var customerEdit = new CustomerEdit
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Email = customer.Email
+            };
+            return View(customerEdit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, CustomerEdit model)
+        {
+            if (id != model.Id || !ModelState.IsValid)
+                return View(ModelState);
+
+            bool wasUpdated = await _service.UpdateCustomer(model);
+            if (wasUpdated) return RedirectToAction(nameof(Index));
+
+            ViewData["ErrorMsg"] = "Unable to save to the database. Please try again later.";
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var customer = _service.GetCustomerById(id);
+            if (customer == null) return NotFound();
+            return View(customer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(CustomerDetail model)
+        {
+            if (await _service.DeleteCustomer(model.Id))
+                return RedirectToAction(nameof(Index));
+            else
+                return BadRequest();
+        }
+    }
+}
---
+++ GeneralStoreMVC/Data/GeneralStoreDbContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GeneralStoreMVC.Data
+{
+    public partial class GeneralStoreDbContext : DbContext
+    {
+        public GeneralStoreDbContext()
+        {
+        }
+
+        public GeneralStoreDbContext(DbContextOptions<GeneralStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<Transaction> Transactions { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("name=ConnectionStrings:GSLocal");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customers", "dev");
+
+                entity.Property(e => e.Email).HasMaxLength(100);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Products", "dev");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Transaction>(entity =>
+            {
+                entity.ToTable("Transactions", "dev");
+
+                entity.Property(e => e.DateOfTransaction).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Transactions)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Transacti__Custo__2B3F6F97");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Transactions)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Transacti__Produ__2A4B4B5E");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
